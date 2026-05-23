58cc919a973b01be7fbbedb098ee195e3beac826
beigestack <m4.sh@tuta.io>
2026-05-24 00:11:18 +0530

    linkपेड़

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<main class="bg-[#0e0b0b] h-screen p-20 text-white">
+<p>good morning</p>
+<p>and in case i dont see ya, </p>
+<p>good afternoon, good evening and good night!</p>
+<br />
+<br />
+<p class="text-gray-500">linkपेड़:</p>
+<a href="https://maniksharma.xyz" class="underline text-gray-300">my website</a><br />
+<a href="https://vncl.xyz" class="underline text-gray-300">my other website</a><br />
+<a href="https://www.instagram.com/m4shing_sm4shing/" class="underline text-gray-300">my instagram</a><br />
+<a href="https://www.youtube.com/@m4shing_sm4shing" class="underline text-gray-300">my youtube</a><br />
+<a href="https://open.spotify.com/user/314h2zmbqq7xslmvyzmstrhhf4ba?si=0b9e5c8a1f2b4c8e" class="underline text-gray-300">my spotify</a>
+</main>
