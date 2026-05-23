3d9d9db90ee224a6b74a21323fcd5f281362bf69
beigestack <m4.sh@tuta.io>
2026-05-24 00:13:37 +0530

    yes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href={favicon} /> <title>linkपेड़</title></svelte:head>
 {@render children()}
