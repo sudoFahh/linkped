a18eaf00796d487ff141972caa3d69780286e81e
beigestack <m4.sh@tuta.io>
2026-05-24 00:22:52 +0530

    yes2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<main class="bg-[#0e0b0b] h-screen p-20 text-white">
+<main class="bg-[#0e0b0b] h-screen p-20 text-white text-5xl leading-15">
 <p>good morning</p>
 <p>and in case i dont see ya, </p>
 <p>good afternoon, good evening and good night!</p>
