ad405b4592e45cfb5ee96990c74ab5b4d62d6504
beigestack <m4.sh@tuta.io>
2026-05-27 20:08:17 +0530

    yes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,50 @@
-<main class="bg-[#0e0b0b] h-screen p-20 text-white text-5xl leading-15">
-<p>good morning!</p>
-<p>and in case i dont see ya, </p>
-<p>good afternoon, good evening and good night!</p>
-<br />
-<br />
-<p class="text-gray-500">linkपेड़:</p>
-<a href="https://maniksharma.xyz" class="underline text-gray-300">my website</a><br />
-<a href="https://vncl.xyz" class="underline text-gray-300">my other website</a><br />
-<a href="https://www.instagram.com/m4shing_sm4shing/" class="underline text-gray-300">my instagram</a><br />
-<a href="https://www.youtube.com/@m4shing_sm4shing" class="underline text-gray-300">my youtube</a><br />
-<a href="https://open.spotify.com/user/314h2zmbqq7xslmvyzmstrhhf4ba?si=0b9e5c8a1f2b4c8e" class="underline text-gray-300">my spotify</a>
+<main class="bg-[#0e0b0b] min-h-screen p-8 md:p-20 text-white">
+	<div class="text-2xl leading-tight md:text-5xl md:leading-15">
+		<p>good morning!</p>
+		<p>and in case i dont see ya,</p>
+		<p>good afternoon, good evening and good night!</p>
+	</div>
+
+	<div class="mt-10 space-y-1 md:space-y-0">
+		<p class="text-gray-500 text-lg md:text-5xl md:leading-15">
+			linkपेड़:
+		</p>
+
+		<div class="flex flex-col text-xl md:text-5xl md:leading-15">
+			<a
+				href="https://maniksharma.xyz"
+				class="underline text-gray-300 hover:text-white transition-colors duration-200"
+			>
+				my website
+			</a>
+
+			<a
+				href="https://vncl.xyz"
+				class="underline text-gray-300 hover:text-white transition-colors duration-200"
+			>
+				my other website
+			</a>
+
+			<a
+				href="https://www.instagram.com/m4shing_sm4shing/"
+				class="underline text-gray-300 hover:text-white transition-colors duration-200"
+			>
+				my instagram
+			</a>
+
+			<a
+				href="https://www.youtube.com/@m4shing_sm4shing"
+				class="underline text-gray-300 hover:text-white transition-colors duration-200"
+			>
+				my youtube
+			</a>
+
+			<a
+				href="https://open.spotify.com/user/314h2zmbqq7xslmvyzmstrhhf4ba?si=0b9e5c8a1f2b4c8e"
+				class="underline text-gray-300 hover:text-white transition-colors duration-200"
+			>
+				my spotify
+			</a>
+		</div>
+	</div>
 </main>
